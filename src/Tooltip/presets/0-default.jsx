ae003847b5ca22ceb07dc81c522e87c8863f2044
Earl Debicki <bartosz.debicki@uxpin.com>
2019-03-26 15:17:01 +0100

    update Tooltip preset by new jsx syntax

--- src/Tooltip/presets/0-default.jsx
+++ src/Tooltip/presets/0-default.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Tooltip from '../Tooltip';
+import Button from '../../Button/Button';
 
 export default (
-    <Tooltip trigger={`<Button>Trigger top placement</Button>`}>This is a small tooltip</Tooltip>
+    <Tooltip
+      trigger={<Button uxpId="tooltip1">Trigger top placement</Button>}
+      uxpId="tooltip0">
+      This is a small tooltip
+    </Tooltip>
 );
